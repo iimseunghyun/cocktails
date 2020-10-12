4405aba498d0ce900d0bba548995ecae965e06aa
ish <cleopearl426@gmail.com>
2020-10-12 14:24:32 +0900

    secured annotation = enabled

--- src/main/java/kr/ish/controller/SecController.java
+++ src/main/java/kr/ish/controller/SecController.java
@@ -1,5 +1,7 @@
 package kr.ish.controller;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,5 +29,19 @@ public class SecController {
 	public void doAdmin() {
 		
 		log.info("...................................get | /admin");
+	}
+	
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MEMBER')")
+	@GetMapping("/annoMember")
+	public void doMember2() {
+		
+		log.info("logined annotation member");
+	}
+	
+	@Secured({"ROLE_ADMIN"})
+	@GetMapping("/annoAdmin")
+	public void doAdmin2() {
+		
+		log.info("admin annotation only");
 	}
 }
